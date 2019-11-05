c78cc6c5ab7f889287d9f0db68bade10d823f862
Jaron Wakley <jdw922@byu.edu>
2019-11-05 00:16:33 +0000

    set up getinterval call

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,31 @@
 var app = new Vue({
   el: '#app',
   data: {
-    instruments: ["Piano", "Clarinet", "Trumpet", "Euphonium", "Harmonica", "Organ"],
-    
+    instruments: ["Piano", "Clarinet", "Trumpet", "Euphonium", "Harmonica", "Organ"]
   },
   methods: {
-
+    GetIntervalREST() {
+      var url = "getinterval?instrument=" + this.instruments[0];
+      fetch(url)
+        .then((data) => {
+          return (data.json());
+        })
+        .then((intervalInfo) => {
+          console.log(intervalInfo);
+          this.PlayNotes(intervalInfo);
+        });
+    },
+    PlayNotes(intervalInfo) {
+        const sleep = (milliseconds) => {
+            return new Promise(resolve => setTimeout(resolve, milliseconds));
+        };
+        this.PlayNote(intervalInfo.note1);
+        sleep(2000).then(() => {
+            this.PlayNote(intervalInfo.note2);
+        });
+    },
+    PlayNote(note) {
+        new Audio("sound/" + note).play();
+    }
   }
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', { root:  'public' });
+});
+
+router.get('/getinterval',async function(req,res,next) {
+    var instrument = req.query.instrument;
+    var note1 = "sound/C4" + instrument + ".mp3";
+    var note2 = "sound/G4" + instrument + ".mp3";
+    var interval = 7;
+    var intervalInfo = {note1:note1, note2:note2, interval:interval};
+    console.log(note1);
+    console.log(intervalInfo);
+    res.status(200).json(intervalInfo);
 });
 
 module.exports = router;
